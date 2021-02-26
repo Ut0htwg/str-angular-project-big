07bd89bb7eb2c460bdb67d971f6da37df8550894
kladya <konkla@gmail.com>
2021-02-26 01:07:55 +0100

    Edit-bill component+toastr, sidebar and routing for all components

---
+++ src/app/page/edit-bill/edit-bill.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Bill } from 'app/model/bill';
+import { BillService } from 'app/service/bill.service';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-edit-bill',
+  templateUrl: './edit-bill.component.html',
+  styleUrls: ['./edit-bill.component.css']
+})
+export class EditBillComponent implements OnInit {
+
+  bill$: Observable<Bill> = this.activatedRoute.params.pipe(
+    switchMap(params => this.billService.get(params.id))
+  );
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private billService: BillService,
+    private router: Router,
+    private toastr: ToastrService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  onUpdate(bill: Bill): void {
+    if (bill.id === 0) {
+      this.billService.create(bill);
+      this.router.navigate(['bills']);
+      this.showSuccess();
+    } else {
+      this.billService.update(bill).subscribe(
+        () => this.router.navigate(['bills'])
+      );
+      this.showInfo();
+    }
+  }
+
+  showSuccess() {
+    this.toastr.success('Sikeres hozzáadás!', 'Üzenet', { timeOut: 3000 })
+  }
+  showInfo() {
+    this.toastr.info('Sikeres módosítás!', 'Üzenet', { timeOut: 3000 })
+  }
+}
