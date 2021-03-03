d749eda4a0fdaa2238422b92c463c18a72402534
kladya <konkla@gmail.com>
2021-03-03 01:51:16 +0100

    Changes from Balazs

--- src/app/model/customer.ts
+++ src/app/model/customer.ts
@@ -1,22 +1,10 @@
 import { Address } from "./address";
 
 export class Customer {
-    [propname: string]: any;
-    id: number = 0;
-    firstName: string = "";
-    lastName: string = "";
-    email: string = "";
+    id = 0;
+    firstName = '';
+    lastName = '';
+    email = '';
     address: Address = new Address();
-    active: boolean = true;
-
-    constructor(properties?: Customer) {
-        if (properties) {
-            this.id = properties.id || 0;
-            this.firstName = properties.firstName || "";
-            this.lastName = properties.lastName || "";
-            this.email = properties.email || "";
-            this.address = properties.address || new Address();
-            this.active = properties.active || true;
-        }
-    }
+    active = false;
 }
--- src/app/service/customer.service.ts
+++ src/app/service/customer.service.ts
@@ -1,46 +1,49 @@
 import { Injectable } from '@angular/core';
 import { Customer } from '../model/customer';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  customerApiUrl: string = 'http://localhost:3000/customers';
-
-  list$: BehaviorSubject<Customer[]> = new BehaviorSubject<Customer[]>([]);
-
-
-  constructor(
-    private http: HttpClient,
-  ) { }
-
-  getAll(): void {
-    this.http.get<Customer[]>(this.customerApiUrl).subscribe(customers => this.list$.next(customers));
-  }
-
-  get(id: number | string): Observable<Customer> {
-    id = parseInt(('' + id), 10);
-    return this.http.get<Customer>(`${this.customerApiUrl}/${id}`);
-  }
-
-  create(customer: Customer): Observable<Customer> {
-    return this.http.post<Customer>(this.customerApiUrl, customer);
-  }
-
-  update(customer: Customer): Observable<Customer> {
-    return this.http
-      .patch<Customer>(`${this.customerApiUrl}/${customer.id}`, customer)
-      .pipe(tap(() => this.getAll()));
-  }
-
   remove(id: number | string): void {
-    id = parseInt(('' + id), 10);
-    this.http.delete<Customer>(`${this.customerApiUrl}/${id}`).subscribe(
+    id = parseInt(("" + id), 10);
+    this.http.delete<Customer>(`${this.apiUrl}/${id}`).subscribe(
       () => this.getAll()
     );
   }
+
+  list$: BehaviorSubject<Customer[]> = new BehaviorSubject<Customer[]>([]);
+  apiUrl = 'http://localhost:3000/customers';
+
+  constructor(private http: HttpClient) {
+    this.getAll();
+  }
+
+  getAll(): void {
+    this.http.get<Customer[]>(this.apiUrl).subscribe(
+      customers => this.list$.next(customers)
+    );
+  }
+
+  get(id: number): Observable<Customer> {
+    id = typeof id === 'string' ? parseInt(id, 10) : id;
+    const ev: Observable<Customer> | undefined = this.http.get<Customer>(`${this.apiUrl}/${id}`);
+    if (id == 0) {
+      return of(new Customer());
+    } else {
+      return ev;
+    }
+  }
+
+  create(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(this.apiUrl, customer);
+  }
+
+  update(customer: Customer): Observable<Customer> {
+    return this.http.patch<Customer>(`${this.apiUrl}/${customer.id}`, customer);
+  }
+
 }
