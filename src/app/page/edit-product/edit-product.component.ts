0e575ad8432dc5a5a4974cf49710d14b9713b8a2
Gootyt <gootyt@gmail.com>
2021-03-03 17:56:18 +0100

    edit product useless

--- src/app/page/edit-product/edit-product.component.ts
+++ src/app/page/edit-product/edit-product.component.ts
@@ -1,4 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Product } from '../../model/product';
+import { ProductService } from '../../service/product.service';
+import { debounceTime, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-product',
@@ -6,10 +14,96 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-product.component.css']
 })
 export class EditProductComponent implements OnInit {
+  product: Product = new Product();
+  updating: boolean = false;
 
-  constructor() { }
+  chosenProduct: Product = new Product();
 
-  ngOnInit(): void {
+  entityName: string = 'product';
+  list$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router,
+    private http: HttpClient
+  ) {
+
   }
 
+  search = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(300),
+      switchMap((txt) => this.like('id', txt))
+    );
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) =>
+      this.productService.get(params.id).subscribe((product) => {
+        console.log(product);
+        this.product = product || new Product();
+        this.product.status = this.product.id ? this.product.active : 'new';
+      })
+    );
+    this.chosenProduct.id = this.product.id;
+  }
+
+  productResultFormatter(product: Product): string {
+    return `${product.id}`;
+  }
+
+  productIputFormatter(product: Product): string {
+    if (!product.id) {
+      return '';
+    }
+    return `(${product.id})`;
+  }
+
+  like(key: string, value: string, limit: number = 10): Observable<Product[]> {
+    key = `${key}_like`;
+    const query = `${this.productService.productApiUrl}/${this.entityName}?${key}=${value}&_limit=${limit}`;
+    return this.http.get<Product[]>(query);
+  }
+
+  onFormSubmit(form: NgForm): void {
+    this.updating = true;
+    this.productService
+      .update(this.product)
+      .subscribe(() => this.router.navigate(['products']));
+  }
+
+  setProductToDatabase(product: Product): void {
+    this.updating = true;
+    product.id = Number(product.id);
+    if (product.id === 0) {
+      this.productService.create(product).subscribe(
+        () => {
+          this.toastr.success('Sikeresen létrehozott termék!', 'Siker!', {
+            timeOut: 3000,
+          });
+          this.updating = false;
+          this.router.navigate(['products']);
+        },
+        (error) =>
+          this.toastr.error('Hiba a termék létrehozásakor!', 'Hiba!', {
+            timeOut: 3000,
+          })
+      );
+    } else {
+      this.productService.update(product).subscribe(
+        () => {
+          this.toastr.success('Sikeresen módítottad a terméket!', 'Siker!', {
+            timeOut: 3000,
+          });
+          this.updating = false;
+          this.router.navigate(['products']);
+        },
+        (error) =>
+          this.toastr.error('Hiba történt a számla módosításakor!', 'Hiba!', {
+            timeOut: 3000,
+          })
+      );
+    }
+  }
 }
