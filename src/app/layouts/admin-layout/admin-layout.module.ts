1df5524d8f9e07752d504a18e7841d14439b46de
MqMaqus <39221161+MqMaqus@users.noreply.github.com>
2021-03-06 20:39:27 +0100

    Feat.: charts on dashboard

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -17,6 +17,8 @@ import {MatRippleModule} from '@angular/material/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatSelectModule} from '@angular/material/select';
+import { ChartComponent } from 'app/components/chart/chart.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
@@ -30,6 +32,7 @@ import {MatSelectModule} from '@angular/material/select';
     MatInputModule,
     MatSelectModule,
     MatTooltipModule,
+    ChartsModule
   ],
   declarations: [
     DashboardComponent,
@@ -40,6 +43,8 @@ import {MatSelectModule} from '@angular/material/select';
     MapsComponent,
     NotificationsComponent,
     UpgradeComponent,
+    ChartComponent,
+    ChartsModule
   ]
 })
 
